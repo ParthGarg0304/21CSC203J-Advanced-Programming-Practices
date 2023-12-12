464477457757f8f4c6bd20f192e00e5745c002fc
Parth Garg <114015879+ParthGarg0304@users.noreply.github.com>
2023-12-12 18:07:15 +0530

    Create Program 6.java

---
+++ Week 6 Object-Oriented Programming Paradigm/Program 6.java
@@ -0,0 +1,69 @@
+/*program to create an interface Sortable with a method sort() that sorts an array of integers in ascending order. Create two classes BubbleSort and SelectionSort that implement the Sortable interface and provide their own implementations of the sort() method.*/
+
+
+import java.util.Scanner;
+
+interface Sortable {
+    void sort(int[] arr);
+}
+
+class BubbleSort implements Sortable {
+    @Override
+    public void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+class SelectionSort implements Sortable {
+    @Override
+    public void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap arr[i] and arr[minIndex]
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+
+        Sortable bubbleSort = new BubbleSort();
+        bubbleSort.sort(arr);
+        System.out.println("Sorted using Bubble Sort:");
+        printArray(arr);
+
+        int[] arr2 = { 64, 25, 12, 22, 11 };
+
+        Sortable selectionSort = new SelectionSort();
+        selectionSort.sort(arr2);
+        System.out.println("\nSorted using Selection Sort:");
+        printArray(arr2);
+    }
+
+    static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
